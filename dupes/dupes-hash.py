728f1118013aa83381981f53193c857ec9a9d1c7
julian <julian@mbp-julian.local>
2020-08-26 15:53:14 -0700

    init setup

---
+++ dupes/dupes-hash.py
@@ -0,0 +1,6 @@
+#in short, it's to write a function that looks for files with duplicate contents and flags them as the sample below:
+#
+#def find_duplicates(rootpath)
+#-> [['a.txt', 'foo/c.txt'], ['b.txt', 'foo/bar/e.txt']]
+#
+#the files live in a directory structure like this one:
